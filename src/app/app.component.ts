e962173201bbf549a0afb31a77c907edc9f2703a
finncbn <finncbn@gmail.com>
2017-02-14 16:12:13 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import {
+  AngularFire,
+  AuthProviders,
+  AuthMethods,
+  FirebaseListObservable
+} from 'angularfire2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  items: FirebaseListObservable<any>;
+  name: any;
+  messageVal: string = '';
+
+  constructor(public ngFire: AngularFire) {
+    this.items = ngFire.database.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    this.ngFire.auth.subscribe(auth => {
+      if (auth) {
+        this.name = auth;
+      }
+    });
+  }
+
+  login() {
+    this.ngFire.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    })
+  }
+
+  chatSend(theMessage: string) {
+    this.items.push({ message: theMessage, name: this.name.facebook.displayName });
+    this.messageVal = '';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCHwsquncw1q36mJI1728y8CYKuEoWOLYs",
+  authDomain: "ng-chat-9fb08.firebaseapp.com",
+  databaseURL: "https://ng-chat-9fb08.firebaseio.com",
+  storageBucket: "ng-chat-9fb08.appspot.com",
+  messagingSenderId: "617714781708"
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
